5a502f640ee72e6f54cc7ef49d331d01e01e27de
曹琬姗 <634068240@qq.com>
2022-04-17 00:09:55 +0800

    hello spring

--- spring-02-hellospring/src/test/java/MyTest.java
+++ spring-02-hellospring/src/test/java/MyTest.java
@@ -1,14 +1,13 @@
-import com.sicilly.service.UserService;
+import com.sicilly.pojo.Hello;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MyTest {
     public static void main(String[] args) {
-        // 获取ApplicationContext,拿到Spring的容器
+        // 获取spring的上下文对象
         ApplicationContext context = new ClassPathXmlApplicationContext("beans.xml");
-        // 容器在手，天下我有，需要什么就get什么
-        UserService userServiceImpl = (UserService)context.getBean("UserServiceImpl");
-        userServiceImpl.getUser();
-
+        // 没有new 而是从容器中取出的hello
+        Hello hello = (Hello)context.getBean("hello");
+        System.out.println(hello.toString());
     }
 }
